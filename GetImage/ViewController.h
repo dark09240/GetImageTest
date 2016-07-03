0474b6cb6cd3e7201831c73f496b990df2a81006
Dark <dark09240@gmail.com>
2016-07-03 16:06:00 +0800

    Initial Commit

---
+++ GetImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetImage
+//
+//  Created by Lycodes_Dong on 7/3/16.
+//  Copyright © 2016 Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
